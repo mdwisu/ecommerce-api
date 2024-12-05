2e96a37e85bb91b61f38dfcba2196ca127f22b93
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-05 23:40:27 +0700

    create and find product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  create(
+    @Body()
+    body: {
+      name: string;
+      price: number;
+      description: string;
+      category: string;
+    },
+  ) {
+    return this.productsService.createProduct(
+      body.name,
+      body.price,
+      body.description,
+      body.category,
+    );
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Body() body: { id: number }) {
+    return this.productsService.findOne(body.id);
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
+
+  createProduct(
+    name: string,
+    price: number,
+    description: string,
+    category: string,
+  ): Promise<Product> {
+    const newProduct = this.productsRepository.create({
+      name,
+      price,
+      description,
+      category,
+    });
+    return this.productsRepository.save(newProduct);
+  }
+
+  findAll(): Promise<Product[]> {
+    return this.productsRepository.find();
+  }
+
+  findOne(id: number): Promise<Product | null> {
+    return this.productsRepository.findOneBy({ id });
+  }
+}
