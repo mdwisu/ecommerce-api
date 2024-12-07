2a46a4f1237d87aa3a769c626d0240c94aab6082
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-07 07:38:31 +0700

    feat: add func addProductToCart, getCartByUser, getCartByUser to module cart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,7 @@
 import {
   Body,
   Controller,
-  Get,
+  Patch,
   Post,
   Request,
   UseGuards,
@@ -9,6 +9,7 @@ import {
 import { CartService } from './cart.service';
 import { ProductsService } from '../products/products.service';
 import { JwtAuthGuard } from '../guards/auth.guard';
+import { Cart } from './cart.entity';
 
 @Controller('cart')
 export class CartController {
@@ -16,4 +17,39 @@ export class CartController {
     private readonly cartService: CartService,
     private readonly productsService: ProductsService,
   ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('')
+  async createCart(
+    @Request() req: any,
+    @Body('status') status: string,
+  ): Promise<Cart> {
+    return this.cartService.createCart(req.user.id, status);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('add-product')
+  async addProductToCart(
+    @Request() req: any,
+    @Body('cartId') cartId: number,
+    @Body('productId') productId: number,
+  ): Promise<Cart> {
+    return this.cartService.addProductToCart(cartId, productId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('get-cart')
+  async getCartByUser(@Request() req: any): Promise<Cart[]> {
+    return this.cartService.getCartByUserId(req.user.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('update-status')
+  async updateCartStatus(
+    @Request() req: any,
+    @Body('cartId') cartId: number,
+    @Body('status') status: string,
+  ) {
+    return this.cartService.updateCartStatus(cartId, status);
+  }
 }
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,4 +1,3 @@
-import { ProductsService } from './../products/products.service';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '../users/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -18,5 +17,39 @@ export class CartService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  
+  async createCart(userId: number, status: string): Promise<Cart> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const cart = new Cart();
+    cart.status = status;
+    cart.user = user;
+    return this.cartRepository.save(cart);
+  }
+
+  async addProductToCart(cartId: number, productId: number): Promise<Cart> {
+    const cart = await this.cartRepository.findOne({
+      where: { id: cartId },
+      relations: ['products'],
+    });
+    cart.products.push(
+      await this.productRepository.findOne({ where: { id: productId } }),
+    );
+    return this.cartRepository.save(cart);
+  }
+
+  async getCartByUserId(userId: number): Promise<Cart[]> {
+    return this.cartRepository.find({
+      where: { user: { id: userId } },
+      relations: ['products'],
+    });
+  }
+
+  async updateCartStatus(cartId: number, status: string): Promise<Cart> {
+    await this.cartRepository.update({ id: cartId }, { status: status });
+    return this.cartRepository.findOne({ where: { id: cartId } });
+  }
 }
